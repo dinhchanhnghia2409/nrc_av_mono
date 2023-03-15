4cbcf38b009eb25d43025c9213856a9e2be8470e
Nguyễn Hoàng Lộc <75782505+niskyB@users.noreply.github.com>
2023-03-15 13:46:37 +0700

    remove unuse validate in user Schema

--- packages/nrc_av_web_api/src/core/models/user.ts
+++ packages/nrc_av_web_api/src/core/models/user.ts
@@ -1,6 +1,6 @@
-import joi from 'joi';
-import { Column, Entity } from 'typeorm';
-import { BaseModel } from './base';
+import joi from "joi";
+import { Column, Entity } from "typeorm";
+import { BaseModel } from "./base";
 @Entity()
 export class User extends BaseModel {
   @Column()
@@ -11,6 +11,6 @@ export class User extends BaseModel {
 }
 
 export const userValidateSchema = {
-  username: joi.string().min(3).max(40).trim().lowercase().required(),
-  password: joi.string().min(6).max(32).trim().alphanum().required()
+  username: joi.string().min(3).max(40).trim().required(),
+  password: joi.string().min(6).max(32).trim().required(),
 };
